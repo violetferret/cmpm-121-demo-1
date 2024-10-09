d65916d9fc01c861973f65a4e601a4dd0d4ac3ca
violetferret <kahlstro@ucsc.edu>
2024-10-09 16:22:50 -0700

    step 1 complete: added button with symbol

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,9 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// create button
+const symbol: string = "🦐";
+const button = document.createElement("button");
+button.innerHTML = symbol;
+app.append(button);
